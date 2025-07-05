5c0f176456b75f6604e2ab904f861a8646b1136e
Nik Tangdit <blackbeltdino@gmail.com>
2025-07-05 12:22:44 -0500

    Spring Init

---
+++ bikeshed/src/main/java/com/bikeshed/bikeshed/BikeshedApplication.java
@@ -0,0 +1,13 @@
+package com.bikeshed.bikeshed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeshedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeshedApplication.class, args);
+	}
+
+}
---
+++ bikeshed/src/test/java/com/bikeshed/bikeshed/BikeshedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikeshed.bikeshed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeshedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
