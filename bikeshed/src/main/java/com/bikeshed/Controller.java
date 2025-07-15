5a30f1b2c3877c012e52385486bc63e8e50855cd
ntangdit <ntangdit@gmail.com>
2025-07-14 23:03:58 -0500

    Controller with endpoint

--- bikeshed/src/main/java/com/bikeshed/Controller.java
+++ bikeshed/src/main/java/com/bikeshed/Controller.java
@@ -1,13 +1,25 @@
 package com.bikeshed;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bikeshed.data.entity.User;
+import com.bikeshed.data.repository.UserRepository;
+
 @RestController
+@RequestMapping("/user")
 public class Controller {
 
-  @GetMapping("/health")
-  public String getString() {
-    return "Server is up";
+  private final UserRepository userRepository;
+
+  public Controller(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @GetMapping
+  public User getUser(@RequestParam("name") String name) {
+    return this.userRepository.findByNameIgnoreCase(name);
   }
 }
--- bikeshed/src/main/java/com/bikeshed/data/repository/UserRepository.java
+++ bikeshed/src/main/java/com/bikeshed/data/repository/UserRepository.java
@@ -1,11 +1,9 @@
 package com.bikeshed.data.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bikeshed.data.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
-  Optional<User> findByNameIgnoreCase(String name);
+  User findByNameIgnoreCase(String name);
 }
